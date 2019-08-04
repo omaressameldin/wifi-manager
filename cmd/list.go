057eda59824e4a48cbbe909d4e5d55a1347cb5d6
Omar Essam (Inpu) <o.essameldin@gmail.com>
2019-08-04 17:24:23 +0200

    Add list command

---
+++ cmd/list.go
@@ -0,0 +1,107 @@
+package cmd
+
+import (
+	"fmt"
+	"os/exec"
+	"regexp"
+	"strings"
+
+	"github.com/omaressameldin/wifi-selector/utils"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+)
+
+func parseFlags(flags *pflag.FlagSet) string {
+	var cmdColumns []string
+	n, _ := flags.GetBool("name")
+	if n {
+		cmdColumns = append(cmdColumns, "ssid")
+	}
+	m, _ := flags.GetBool("mode")
+	if m {
+		cmdColumns = append(cmdColumns, "mode")
+	}
+	c, _ := flags.GetBool("chan")
+	if c {
+		cmdColumns = append(cmdColumns, "chan")
+	}
+	r, _ := flags.GetBool("rate")
+	if r {
+		cmdColumns = append(cmdColumns, "rate")
+	}
+	g, _ := flags.GetBool("signal")
+	if g {
+		cmdColumns = append(cmdColumns, "signal")
+	}
+	b, _ := flags.GetBool("bars")
+	if b {
+		cmdColumns = append(cmdColumns, "bars")
+	}
+	s, _ := flags.GetBool("security")
+	if s {
+		cmdColumns = append(cmdColumns, "security")
+	}
+	u, _ := flags.GetBool("in-use")
+	if u {
+		cmdColumns = append(cmdColumns, "in-use")
+	}
+	if len(cmdColumns) > 0 {
+		return fmt.Sprintf("-f %v", strings.Join(cmdColumns, ","))
+	} else {
+		return ""
+	}
+}
+
+func renderNetworks(flags *pflag.FlagSet, networks []string) {
+	u, _ := flags.GetBool("in-use")
+
+	for i, n := range networks {
+		num := ""
+		if i == len(networks)-1 {
+			break
+		}
+
+		if i == 0 {
+			fmt.Println(n)
+			continue
+		} else if u {
+			isMatched, _ := regexp.MatchString("(.*)(\\s)+\\*", n)
+			if isMatched {
+				fmt.Println(n)
+				break
+			}
+		} else {
+			num = fmt.Sprintf("%d-", i)
+			fmt.Println(num, n)
+		}
+	}
+}
+
+var listCmd = &cobra.Command{
+	Use:       "list",
+	Short:     "List wifi networks",
+	ValidArgs: []string{},
+
+	Run: func(cmd *cobra.Command, args []string) {
+		flags := cmd.Flags()
+		command := fmt.Sprintf("nmcli %v dev wifi list", parseFlags(flags))
+		c := exec.Command("bash", "-c", command)
+		o, err := c.Output()
+		utils.Must(err)
+
+		networks := strings.Split(string(o), "\n")
+		renderNetworks(flags, networks)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(listCmd)
+	listCmd.Flags().BoolP("name", "n", false, "show only networks' name(ssid)")
+	listCmd.Flags().BoolP("mode", "m", false, "show only networks' mode")
+	listCmd.Flags().BoolP("chan", "c", false, "show only networks' chan")
+	listCmd.Flags().BoolP("rate", "r", false, "show only networks' rate")
+	listCmd.Flags().BoolP("signal", "g", false, "show only networks' signal")
+	listCmd.Flags().BoolP("bars", "b", false, "show only networks' bars")
+	listCmd.Flags().BoolP("security", "s", false, "show only networks' security")
+	listCmd.Flags().BoolP("in-use", "u", false, "show only used network")
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/omaressameldin/wifi-selector
+
+go 1.12
+
+require (
+	github.com/spf13/cobra v0.0.5
+	github.com/spf13/pflag v1.0.3
+)
