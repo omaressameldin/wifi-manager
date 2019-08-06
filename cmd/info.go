ead88d7b8b9730a6bd9f8d474d1a5adac2be01f5
Omar Essam (Inpu) <o.essameldin@gmail.com>
2019-08-06 22:14:43 +0200

    Add password, main & connection flags to info cmd

--- cmd/info.go
+++ cmd/info.go
@@ -3,20 +3,52 @@ package cmd
 import (
 	"fmt"
 	"os/exec"
+	"strings"
 
 	"github.com/omaressameldin/wifi-selector/network"
 	"github.com/omaressameldin/wifi-selector/utils"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
-func showInfo(selectedNet network.Network) {
+func parseInfoFlags(flags *pflag.FlagSet) string {
+	var infoToSho []string
+
+	p, _ := flags.GetBool("password")
+	if p {
+		infoToSho = append(infoToSho, "'\\[wifi-security\\]'")
+	}
+
+	m, _ := flags.GetBool("main")
+	if m {
+		infoToSho = append(infoToSho, "'\\[wifi\\]'")
+	}
+
+	c, _ := flags.GetBool("connection")
+	if c {
+		infoToSho = append(infoToSho, "'\\[connection\\]'")
+	}
+
+	if len(infoToSho) > 0 {
+		return fmt.Sprintf("| grep -A 4 -E %v", strings.Join(infoToSho, "\\|"))
+	} else {
+		return ""
+	}
+}
+
+func showInfo(selectedNet network.Network, extraOptions string) {
 	c := exec.Command(
 		"bash",
 		"-c",
-		fmt.Sprintf("sudo cat /etc/NetworkManager/system-connections/\"%v\"", selectedNet.Filename),
+		fmt.Sprintf(
+			"sudo cat /etc/NetworkManager/system-connections/\"%v\" %v",
+			selectedNet.Filename,
+			extraOptions,
+		),
 	)
 	o, err := c.Output()
 	utils.Must(err)
+
 	fmt.Println(string(o))
 }
 
@@ -30,10 +62,15 @@ var infoCmd = &cobra.Command{
 			wifiLists,
 			"👀",
 		)
-		showInfo(wifiLists[selected])
+		flags := cmd.Flags()
+
+		showInfo(wifiLists[selected], parseInfoFlags(flags))
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(infoCmd)
+	infoCmd.Flags().BoolP("password", "p", false, "show only networks' password")
+	infoCmd.Flags().BoolP("main", "m", false, "show only networks' main info")
+	infoCmd.Flags().BoolP("connection", "c", false, "show only networks' connection")
 }
